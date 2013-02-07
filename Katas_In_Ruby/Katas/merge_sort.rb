e047f3299fe3173864b3763174119a72879cb9d8
Sumeet Kumar <kr.sumeet@gmail.com>
2013-02-07 14:43:18 -0800

    tests are made to pas

--- Katas_In_Ruby/Katas/merge_sort.rb
+++ Katas_In_Ruby/Katas/merge_sort.rb
@@ -2,14 +2,14 @@ class MergeSort
 
   def self.merge_sort(array)
 
-    if array.length==1
-      return
+    if array.length <= 1
+      return array
     end
 
-    mid = (array.length -1)/2
+    mid = (array.length - 1)/2
 
     left = array[0..mid]
-    right = array[mid..array.length]
+    right = array[mid+1..array.length-1]
 
     return merge_sorted_arrays(merge_sort(left), merge_sort(right))
 
@@ -18,28 +18,30 @@ class MergeSort
   def self.merge_sorted_arrays(left_array, right_array)
 
     #returns merged array
-    total_length=left_array.length+right_array.length
-    merged_array =[total_length]
+    total_length = left_array.length + right_array.length
+    merged_array = Array.new
 
-    left_index, right_index =0
-    for i in 0..total_length-1
+    i = 0
+    left_index = 0
+    right_index = 0
+    for i in 0..(total_length-1)
 
       if left_index == left_array.length
-        merged_array << right_array[right_index..right_array.length-1]
+        merged_array.concat(right_array[right_index..right_array.length-1])
         break;
       end
 
       if right_index == right_array.length
-        merged_array << left_array[left_index..right_array.length-1]
+        merged_array.concat(left_array[left_index..left_array.length-1])
         break;
       end
 
       if left_array[left_index] <= right_array[right_index]
         merged_array[i] = left_array[left_index]
-        ++left_index
+        left_index += 1
       else
         merged_array[i] = right_array[right_index]
-        ++right_index
+        right_index += 1
       end
     end
 
--- Katas_In_Ruby/spec/merge_sort_spec.rb
+++ Katas_In_Ruby/spec/merge_sort_spec.rb
@@ -5,9 +5,20 @@ describe "MergeSort" do
 
   it "merge_sorted_arrays should merge two sorted arrays" do
 
-    left = [2, 5, 8, 9]
+    left = [2, 5, 8, 9, 13]
     right = [1, 4, 6, 7]
-    MergeSort.merge_sorted_arrays(left, right)
+    merged_array = MergeSort.merge_sorted_arrays(left, right)
+
+    merged_array.should == [1, 2, 4, 5, 6, 7, 8, 9, 13]
+
+  end
+
+  it "should sort array" do
+    array = [2, 1, 6, 4, 2, 9, 14, 3, 9, 0, -4]
+
+    sorted_array = MergeSort.merge_sort(array)
+
+    sorted_array.should == [-4, 0, 1, 2, 2, 3, 4, 6, 9, 9, 14]
 
   end
 end
