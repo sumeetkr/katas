4e4467717570e466618061d0905a4bce2da21d46
Sumeet Kumar <kr.sumeet@gmail.com>
2013-02-11 00:24:56 -0800

    Added maximum element in unimodal array

--- Katas_In_Ruby/Katas/array_problems.rb
+++ Katas_In_Ruby/Katas/array_problems.rb
@@ -22,11 +22,56 @@ class ArrayProblems
 
   end
 
-  #  Problem description
-  #  You are a given a unimodal array of n distinct elements,
-  #  meaning that its entries are in increasing order up until its maximum element,
-  #  after which its elements are in decreasing order. Give an algorithm to compute
-  #  the maximum element that runs in O(log n) time.
 
+  def self.get_max_element_of_unimodal_array(array)
+
+    #  Problem description
+    #  You are a given a unimodal array of n distinct elements,
+    #  meaning that its entries are in increasing order up until its maximum element,
+    #  after which its elements are in decreasing order. Give an algorithm to compute
+    #  the maximum element that runs in O(log n) time.
+
+    current_index = (array.length - 1)/2
+    last_left_index = 0
+    last_right_index = array.length - 1
+    slope = 1
+    last_slope = 1
+
+    #  if slope is negative, traverse in left direction, else move right
+    #  in every step remove half of the array
+
+    until slope ==0
+
+      if (slope > 0)
+        last_left_index = current_index
+        current_index = (current_index + last_right_index)/2
+      else
+        last_right_index = index
+        current_index = (current_index + last_left_index)/2
+      end
+
+      last_slope = slope
+      slope = get_slope(array, current_index)
+    end
+
+    return array[current_index]
+  end
+
+  def self.get_slope(array, index)
+    #return -1 (negative), 1 (positive), 0
+    mid = array[index]
+    left = array[index -1]
+    right = array[index +1]
+
+    if (left < mid && mid < right)
+      return 1
+    else
+      if (left > mid && mid > right)
+        return 1
+      else
+        return 0
+      end
+    end
+  end
 
 end
--- Katas_In_Ruby/spec/array_problems_spec.rb
+++ Katas_In_Ruby/spec/array_problems_spec.rb
@@ -1,5 +1,5 @@
 require "spec_helper"
-require "../Katas/array_sorters"
+require "../Katas/array_problems"
 
 describe "My behaviour" do
 
@@ -9,4 +9,21 @@ describe "My behaviour" do
     indices_array[0].should == 2
     indices_array[1].should == 4
   end
+
+  it "should return the maximum element of unimodal array" do
+    array = [2, 3, 5, 8, 6, 1]
+
+    max_element = ArrayProblems.get_max_element_of_unimodal_array(array)
+    max_element.should == 8
+
+  end
+
+  it "should return the maximum element of unimodal array" do
+    array = [2, 3, 4, 8, 6, 3, 1, -2, -6, -8, -9]
+
+    max_element = ArrayProblems.get_max_element_of_unimodal_array(array)
+    max_element.should == 8
+
+  end
+
 end
