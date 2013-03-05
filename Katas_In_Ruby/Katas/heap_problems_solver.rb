cc5eff431c98bc8a1e80ef30518f4abc0679c721
Sumeet Kumar <kr.sumeet@gmail.com>
2013-03-05 00:57:59 -0800

    Added  find median in logarithmic time

--- Katas_In_Ruby/Katas/heap_problems_solver.rb
+++ Katas_In_Ruby/Katas/heap_problems_solver.rb
@@ -1,12 +1,71 @@
 class HeapProblemsSolver
 
+  def self.find_median_of_an_array_of_nodes_in_ith_step(sequence_of_nodes)
+    #  You are givien a sequence of numbers X1, X2, X3, ...., Xi one by one. At any time (or step i),
+    # you need to return the median of {X1, X2, .... , Xi}.
+    # Constraint: Your program should not take more than Log(i) time for i-th step.
+
+    find_median_solver = FindMedianSolver.new
+    last_median = nil
+
+    for i in 0..(sequence_of_nodes.length - 1)
+      last_median = find_median_solver.insert_new_node_and_return_median_node(sequence_of_nodes[i])
+
+    end
+
+    return last_median
+  end
+
+end
+
+class FindMedianSolver
+  def initialize
+    @min_heap = Heap.new()
+    @max_heap = Heap.new(false)
+  end
+
+  def insert_new_node_and_return_median_node(node)
+    #boundary case, first insert
+    if (@min_heap.count == 0)
+      @min_heap.insert(node)
+      return node
+    end
+
+    median_node = nil
+
+    if (node.key < @min_heap.find_root_node.key)
+      @max_heap.insert(node)
+      median_node = @max_heap.find_root_node
+    else
+      @min_heap.insert(node)
+      median_node = @min_heap.find_root_node
+    end
+
+
+    # the two heaps should be of same size (or 1 size difference), to return median correctly
+    if ((@min_heap.count - @max_heap.count).abs > 1)
+      if (@min_heap.count > @max_heap.count)
+        @max_heap.insert(@min_heap.get_root_node)
+
+        median_node = @max_heap.find_root_node
+      else
+        @min_heap.insert(@max_heap.get_root_node)
+        median_node = @min_heap.find_root_node
+      end
+
+    end
+
+    return median_node
+  end
 end
 
 class Heap
   @internal_array_of_nodes
+  @is_min_heap
 
-  def initialize
+  def initialize(is_min_heap = true)
     @internal_array_of_nodes = []
+    @is_min_heap = is_min_heap
   end
 
   def insert(node)
@@ -17,11 +76,11 @@ class Heap
     bubble_up(child_index)
   end
 
-  def find()
+  def find_root_node()
     return @internal_array_of_nodes[0]
   end
 
-  def get()
+  def get_root_node()
     min_node = @internal_array_of_nodes[0]
 
     last_index = @internal_array_of_nodes.length - 1
@@ -33,6 +92,15 @@ class Heap
     return min_node
   end
 
+  def compare(first_node, second_node)
+    if (@is_min_heap)
+      return first_node.key < second_node.key
+    else
+      return first_node.key > second_node.key
+    end
+
+  end
+
   def count
     return @internal_array_of_nodes.length
   end
@@ -42,10 +110,9 @@ class Heap
     is_heap_property_satisfied = true
     count = @internal_array_of_nodes.length - 1
     for current_index in count..1
-      if (@internal_array_of_nodes[current_index] < @internal_array_of_nodes[(current_index -1)/ 2])
+      if compare(@internal_array_of_nodes[current_index], @internal_array_of_nodes[(current_index -1)/ 2])
         is_heap_property_satisfied = false
       end
-
       current_index -= 1
     end
 
@@ -58,7 +125,7 @@ class Heap
     while child_index > 0
       parent_index = (child_index-1)/2
 
-      if (@internal_array_of_nodes[child_index].key < @internal_array_of_nodes[parent_index].key)
+      if compare(@internal_array_of_nodes[child_index], @internal_array_of_nodes[parent_index])
         #  swap parent and child nodes
         temp = @internal_array_of_nodes[child_index]
         @internal_array_of_nodes[child_index] = @internal_array_of_nodes[parent_index]
@@ -78,9 +145,9 @@ class Heap
 
 
     while first_child_index < last_index && second_child_index < last_index
-      min_child_index = @internal_array_of_nodes[first_child_index].key < @internal_array_of_nodes[second_child_index].key ? first_child_index : second_child_index
+      min_child_index = compare(@internal_array_of_nodes[first_child_index], @internal_array_of_nodes[second_child_index]) ? first_child_index : second_child_index
       #if min child is more than parent, then swap
-      if (@internal_array_of_nodes[min_child_index].key < @internal_array_of_nodes[parent_index].key)
+      if compare(@internal_array_of_nodes[min_child_index], @internal_array_of_nodes[parent_index])
         #  swap parent and child nodes
         temp = @internal_array_of_nodes[min_child_index]
         @internal_array_of_nodes[min_child_index] = @internal_array_of_nodes[parent_index]
--- Katas_In_Ruby/spec/heap_problems_solver_spec.rb
+++ Katas_In_Ruby/spec/heap_problems_solver_spec.rb
@@ -23,7 +23,7 @@ describe "Heap problems behaviour" do
 
     it "find, finds min node" do
       lambda {
-        subject.find
+        subject.find_root_node
       }.should change(@heap, :count).by(0)
     end
 
@@ -39,14 +39,14 @@ describe "Heap problems behaviour" do
       heap.insert(Node.new(0))
 
       lambda {
-        node = heap.get
+        node = heap.get_root_node
         node.key.should == 0
       }.should change(heap, :count).by(-1)
 
       heap.is_heap.should be_true
 
       lambda {
-        node = heap.get
+        node = heap.get_root_node
         node.key.should == 1
       }.should change(heap, :count).by(-1)
 
@@ -63,7 +63,7 @@ describe "Heap problems behaviour" do
       heap.insert(Node.new(10))
       heap.insert(Node.new(0))
 
-      heap.find.key.should == 0
+      heap.find_root_node.key.should == 0
       heap.is_heap.should be_true
     end
 
@@ -75,5 +75,23 @@ describe "Heap problems behaviour" do
 
     it { should respond_to(:key) }
     it { should respond_to(:value) }
+  end
+
+  context "find_median_of_an_array_in_ith_step" do
+
+    it "should return the right median" do
+      sequence = [Node.new(1), Node.new(0), Node.new(10), Node.new(5), Node.new(8),
+                  Node.new(2), Node.new(4), Node.new(12), Node.new(15)]
+      median_node = HeapProblemsSolver.find_median_of_an_array_of_nodes_in_ith_step(sequence)
+      median_node.key.should == 5
+    end
+
+    it "should return the right median" do
+      sequence = [Node.new(1), Node.new(10), Node.new(2), Node.new(9), Node.new(3),
+                  Node.new(8), Node.new(4), Node.new(7), Node.new(5)]
+      median_node = HeapProblemsSolver.find_median_of_an_array_of_nodes_in_ith_step(sequence)
+      median_node.key.should == 5
+    end
+
   end
 end
