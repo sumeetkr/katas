3d320a049a75a95bef3d35fdd0a8981c9d94edfa
Sumeet Kumar <kr.sumeet@gmail.com>
2013-05-07 22:47:14 -0700

    solution for bit manioulation

---
+++ Katas_in_Java/Katas/test/BitManipulationTests.java
@@ -0,0 +1,29 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class BitManipulationTests {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindBinaryRepresentationOfANumberInPalindrome(){
+	//Find if a binary representation of a number is palindrome. The function should work 
+	//irrespective of number of bytes for an integer. Suppose if our machine is 4 bytes for
+	//an int, how will you use the program for 8 byte machine. 
+	}
+}
---
+++ Katas_in_Java/Katas/test/TrieTest.java
@@ -0,0 +1,48 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class TrieTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testAddWord() {
+		
+	}
+	
+	@Test
+	public void testCountWords(){
+		
+	}
+	
+	@Test
+	public void testCountPrefixes(){
+		
+	}
+	
+	@Test
+	public void testFindLongestStringMadeOfOtherStrings(){
+	//There are lots of string in a file. Find the longest string that could be made
+	//from the other strings in the file. 
+	//Eg. 
+	//the 
+	//there 
+	//after 
+	//thereafter 
+	//reaf 
+	//
+	//ans. 
+	//thereafter
+	}
+
+}
